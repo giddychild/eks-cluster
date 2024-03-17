765b4479a4f11cbf962fe9d62c10b5fd15a77a01
Seyi <seyiobadee@gmail.com>
2024-03-17 11:46:00 -0500

    updated code

--- eks-kubeflow/providers.tf
+++ eks-kubeflow/providers.tf
@@ -9,11 +9,6 @@ provider "kubernetes" {
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
-    # args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-    args = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.id]
+    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
   }
-}
-
-data "aws_eks_cluster" "cluster" {
-  name = aws_eks_cluster.eks.id
 }
